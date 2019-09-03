e1b075c3a7dec4c5b9b3c3ef72837631db9f4ef0
alumno <alumno>
2019-09-03 11:57:40 -0300

    se agrego funcion recursiva + la mejor forma

--- 4/clase 5/main.c
+++ 4/clase 5/main.c
@@ -4,6 +4,7 @@
 void funcion(int*);
 
 
+/**
 int main()
 {
     int numero=4;
@@ -17,8 +18,53 @@ int main()
 void funcion(int* pNumero )
 {
 
-                                /**pNumero=100; /**me lleva al valor al que apunta la variable.*/
+                                /pNumero=100; me lleva al valor al que apunta la variable.
     prinft("Ingrese un numero: ");
     scanf("%d",pNumero);
 
+}
+*/
+
+
+/** \brief Calcula el factorial de un numero
+ *
+ * \param int el numero al que se va a factorear
+ * \return int El factorial
+ *
+ */
+int factorial(int);
+
+int main()
+{
+   int numero=5;
+   int resultado; /** para que no tenga basura se le pone "1".  */
+   /*int i;
+
+
+   for(i=numero;i>=1;i--)
+   {
+      resultado=resultado *i;
+   }
+    printf("el numero factorizado es: %d",resultado);*/  /** la mejor forma, pero no la unica, time to funcion recursiva*/
+
+    resultado=factorial(numero);
+    printf("el numero es: %d",resultado);
+
+
+    return 0;
+}
+
+int factorial(int numero)
+{
+    int resultado;
+
+    if(numero==0)
+    {
+        resultado= 1;
+    }
+    else
+    {
+        resultado= numero*factorial(numero-1);
+    }
+    return resultado;
 }
